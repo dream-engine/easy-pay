0eac55544e5ffdf8cd8a7bb1ad3afe643638dc96
Mohit Singh <117419663+mohit-kumar-nickelfox@users.noreply.github.com>
2023-07-10 20:40:03 +0530

    feat(initial commit): add swiftui project

---
+++ easy-pay/easy_payApp.swift
@@ -0,0 +1,17 @@
+//
+//  easy_payApp.swift
+//  easy-pay
+//
+//  Created by Mohit Kumar Singh on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct easy_payApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
